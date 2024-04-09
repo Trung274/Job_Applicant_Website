2e5865b4f1e701fcd8099fe634afadd65ee64098
Trung274 <trunksadboi@gmail.com>
2024-04-09 16:19:36 +0700

    finished bcrypt implementing, jwt, react hook to track user

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
-const cors = require('cors')
+const cors = require('cors');
 const {mongoose} = require('mongoose')
-const app = express();
+const app = express()
+const cookieParser = require('cookie-parser')
+
 //database connection
-mongoose.connect('mongodb://localhost:27017/')
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Database Connected'))
 .catch(() => console.log('Database not connected', err))
 
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
 
 app.use('/', require(`./routes/authRoutes`))
 
